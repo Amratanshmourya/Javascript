2e2a4f1758bcd2930bbb8afc5198ca68a4f58784
Amratanshmourya <168343343+Amratanshmourya@users.noreply.github.com>
2024-05-24 14:58:33 +0000

    methods

---
+++ basic/basic.js
@@ -0,0 +1,4 @@
+let name="Amratansh Mourya";
+let age = 20
+let role="Developer";
+console.log(` i am ${name}, my age is ${age}, i am a ${role}`);
--- basic/objects.js
+++ basic/objects.js
@@ -1,7 +1,20 @@
-let obj={
+let obj= {
     name:"Amratansh Mourya",
     city:"indore",
     email:"AM@gmail.com",
     isLoggedin:"not"
 }
-console.log(obj[name]);
+n=obj["name"]
+// console.log(n);
+// console.log(obj[name]);          wrong syntax because in [] the attributes are written without string;
+console.log(obj.email);
+
+//updating obj data
+obj.email="Amm@gmail.com";
+// console.log(obj.email);
+
+//  to stop  updation on onj try Object.freeze(obj);
+
+Object.freeze(obj)
+obj.email="123@gmail.com";
+// console.log(obj);
