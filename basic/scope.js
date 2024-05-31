97c49d2f2a3b469909d14ea179ec45d5d1f1c1ed
Amratanshmourya <168343343+Amratanshmourya@users.noreply.github.com>
2024-05-31 15:09:10 +0000

    added one function

--- basic/scope.js
+++ basic/scope.js
@@ -6,3 +6,14 @@ var c=3;     //global scope
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username="Amratansh";
+    function two(){}
+        // const id=211;     //local variable can't access outside two() function
+        console.log(username);
+    }
+    two();
+    console.log(id);
+}
+one();
