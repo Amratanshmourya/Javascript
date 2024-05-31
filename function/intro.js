5cbbd691b31b9d82e8d53779f8ec1d208b5cd4ce
Amratanshmourya <168343343+Amratanshmourya@users.noreply.github.com>
2024-05-31 12:33:57 +0000

    new

---
+++ function/intro.js
@@ -0,0 +1,10 @@
+function addtwonumbers(num1,num2)
+{
+    return num1+num2;
+}
+// console.log(addtwonumbers(2,7));
+function welcomemsg(username="Sam"){
+    return `${username} just logged in`
+}
+// console.log(welcomemsg());
+
