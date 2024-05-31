1adc5c5862435b6389ba622741a1fa6bdf19411d
Amratanshmourya <168343343+Amratanshmourya@users.noreply.github.com>
2024-05-31 12:42:21 +0000

    array

--- function/intro.js
+++ function/intro.js
@@ -8,3 +8,20 @@ function welcomemsg(username="Sam"){
 }
 // console.log(welcomemsg());
 
+
+let obj={
+    username:"Amratansh",
+    age:21
+}
+function objuse(myobj)
+{
+    console.log(`the user is : ${myobj.username}, the age is: ${myobj.age}`);
+}
+// objuse(obj)
+function arruse(myarr)
+{
+    console.log(myarr[2]);
+}
+const arr=[1,2,3,4,5,6];
+
+// arruse(arr)
