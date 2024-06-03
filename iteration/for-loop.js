03d45686d70f48f82a42a443129e38cfde48cc60
Amratanshmourya <168343343+Amratanshmourya@users.noreply.github.com>
2024-06-03 13:03:11 +0000

    created

---
+++ iteration/for-loop.js
@@ -0,0 +1,18 @@
+for (let i = 1; i <=20; i++) {
+    if(i==5)
+        {
+            // console.log("5 detected");
+            break;
+        }
+    // console.log(`value of i is ${i}`);    
+    
+}
+for (let i = 1; i <=20; i++) {
+    if(i==5)
+        {
+            console.log("5 detected");
+            continue;
+        }
+    console.log(`value of i is ${i}`);    
+    
+}
