2b9f253e2915cd683f2816921fa9ee048c307263
Amratanshmourya <168343343+Amratanshmourya@users.noreply.github.com>
2024-06-03 12:25:11 +0000

    created

---
+++ control/switch-case.js
@@ -0,0 +1,25 @@
+const month=10
+switch(month)
+{
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    default:
+        console.log("invalid choice"); 
+        break;   
+    
+    
+
+}
